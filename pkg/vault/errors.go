18b3b094fc5b9581da684b884b4ccf1ceacd800f
Alex V Kotelnikov <gudron2s@gmail.com>
2024-10-02 17:17:18 +0400

    feat:crypto-bundle/CBDL#19.Support last lib-errors version

--- pkg/vault/errors.go
+++ pkg/vault/errors.go
@@ -2,43 +2,32 @@ package vault
 
 import (
 	"errors"
-	"fmt"
-)
-
-var (
-	ErrConfigIsNotValid = errors.New("config is not valid.some fields are missing")
 )
 
 const (
-	ErrVaultAPIClientInit  InternalError = "unable to initialize vault client"
-	ErrK8sAuthInit         InternalError = "unable to initialize kubernetes auth method"
-	ErrUserpassInit        InternalError = "unable to initialize userpass auth method"
-	ErrK8sLogin            InternalError = "unable to log in with kubernetes auth"
-	ErrUserpassLogin       InternalError = "unable to log in with user and password"
-	ErrNotExistingAuthInfo InternalError = "no auth info was returned after login"
-	ErrReadSecret          InternalError = "unable to read secret"
-	ErrEmptySecret         InternalError = "unable to get secret"
-	ErrCastSecret          InternalError = "secret casting error"
-	ErrNotExistingKey      InternalError = "missed key in secret"
-	ErrKeyType             InternalError = "unexpected key type in secret"
-	ErrTransitSecretFormat InternalError = "unexpected format for transit secret"
-	ErrInitTokenTTLWatcher InternalError = "unable to initialize auth token lifetime watcher"
-	ErrUnableGetTokenInfo  InternalError = "unable to get token info"
-	ErrEmptyConfig         InternalError = "config is empty"
+	ErrVaultAPIClientInitDetail  = "unable to initialize vault client"
+	ErrK8sAuthInitDetail         = "unable to initialize kubernetes auth method"
+	ErrUserpassInitDetail        = "unable to initialize userpass auth method"
+	ErrK8sLoginDetail            = "unable to log in with kubernetes auth"
+	ErrUserpassLoginDetail       = "unable to log in with user and password"
+	ErrNotExistingAuthInfoDetail = "no auth info was returned after login"
+	ErrReadSecretDetail          = "unable to read secret"
+	ErrEmptySecretDetail         = "unable to get secret"
+	ErrCastSecretDetail          = "secret casting error"
+	ErrNotExistingKeyDetail      = "missed key in secret"
+	ErrKeyTypeDetail             = "unexpected key type in secret"
+	ErrTransitSecretFormatDetail = "unexpected format for transit secret"
+	ErrInitTokenTTLWatcherDetail = "unable to initialize auth token lifetime watcher"
+	ErrUnableGetTokenInfoDetail  = "unable to get token info"
+	ErrEmptyConfigDetail         = "config is empty"
 )
 
-type InternalError string
-
-func (e InternalError) Error() string {
-	return string(e)
-}
-
-// WithMsg returns internal error with additional text.
-func (e InternalError) WithMsg(msg string) error {
-	return fmt.Errorf("%w: %s", e, msg)
-}
-
-// NewInternalError returns new error.
-func NewInternalError(vErr InternalError, err error) error {
-	return fmt.Errorf("%w: %q", vErr, err)
-}
+var (
+	ErrConfigIsNotValid   = errors.New("config is not valid.some fields are missing")
+	ErrReadSecret         = errors.New(ErrReadSecretDetail)
+	ErrEmptySecret        = errors.New(ErrEmptySecretDetail)
+	ErrCastSecret         = errors.New(ErrCastSecretDetail)
+	ErrNotExistingKey     = errors.New(ErrNotExistingKeyDetail)
+	ErrKeyType            = errors.New(ErrKeyTypeDetail)
+	ErrUnableGetTokenInfo = errors.New(ErrUnableGetTokenInfoDetail)
+)
